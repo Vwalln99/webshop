ad3bb8fa8718fe46d134c13ccdc2f12a61bc0ad0
Vwalln99 <wallner.viktoria@gmx.net>
2024-07-01 08:22:47 +0200

    init commit

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+require '../vendor/autoload.php';
+require '../config/database.php';
+
+include '../includes/header.php';
+include '../views/index.view.php';
+include '../includes/footer.php';
---
+++ public/views/index.view.php
@@ -0,0 +1,5 @@
+<main>
+    <h2>Welcome to our Webshop</h2>
+    <?php include 'layouts/index.main.php'; ?>
+    <p>This is the homepage of the webshop.</p>
+</main>
